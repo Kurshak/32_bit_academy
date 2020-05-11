0030b11187d2045ff1842634a59342daf415870d
Иван Гарцуев <l.tisha@mail.ru>
2020-05-11 21:17:59 +0300

    Fix nesting

--- app/controllers/api/v1/answers/get_answers_controller.rb
+++ app/controllers/api/v1/answers/get_answers_controller.rb
@@ -1,17 +1,23 @@
 # frozen_string_literal: true
 
-class GetAnswersController < ApplicationController
-  def by_student
-    task = Tasks.find_by_id(params[:task_id])
-    given_tasks = GivenTasks.where(task_id: task.ids, student_id: params[:student_id])
-    @answers = Answers.where(given_task_id: given_tasks.ids)
-    render json: @answer.to_json
-  end
+module Api
+  module V1
+    module Answers
+      class GetAnswersController < ApplicationController
+        def by_student
+          task = Tasks.find_by_id(params[:task_id])
+          given_tasks = GivenTasks.where(task_id: task.ids, student_id: params[:student_id])
+          @answers = Answers.where(given_task_id: given_tasks.ids)
+          render json: @answer.to_json
+        end
 
-  def by_task
-    task = Tasks.find_by_id(params[:task_id])
-    given_tasks = GivenTasks.where(task_id: task.ids)
-    @answers = Answers.where(given_task_id: given_tasks.ids)
-    render json: @answer.to_json
+        def by_task
+          task = Tasks.find_by_id(params[:task_id])
+          given_tasks = GivenTasks.where(task_id: task.ids)
+          @answers = Answers.where(given_task_id: given_tasks.ids)
+          render json: @answer.to_json
+        end
+      end
+    end
   end
 end
--- app/controllers/api/v1/tasks/get_tasks_controller.rb
+++ app/controllers/api/v1/tasks/get_tasks_controller.rb
@@ -1,36 +1,42 @@
 # frozen_string_literal: true
 
-class GetTasksController < ApplicationController
-  def by_course
-    groups = Course.find_by_id(params[:course_id]).groups
-    @tasks = Task.where(group_id: groups.ids)
-    render json: @tasks.to_json
-  end
+module Api
+  module V1
+    module Tasks
+      class GetTasksController < ApplicationController
+        def by_course
+          groups = Course.find_by_id(params[:course_id]).groups
+          @tasks = Task.where(group_id: groups.ids)
+          render json: @tasks.to_json
+        end
 
-  def by_theme
-    subthemes = Theme.find_by_id(params[:theme_id]).subtheme
-    @tasks = Task.where(subtheme_id: subthemes.ids)
-    render json: @tasks.to_json
-  end
+        def by_theme
+          subthemes = Theme.find_by_id(params[:theme_id]).subtheme
+          @tasks = Task.where(subtheme_id: subthemes.ids)
+          render json: @tasks.to_json
+        end
 
-  def by_subtheme
-    @tasks = Task.where(subtheme_id: params[:subtheme_id])
-    render json: @tasks.to_json
-  end
+        def by_subtheme
+          @tasks = Task.where(subtheme_id: params[:subtheme_id])
+          render json: @tasks.to_json
+        end
 
-  def by_student
-    @tasks = Task.where(user_id: params[:user_id], group_id: params[:user_id])
-    render json: @task.to_json
-  end
+        def by_student
+          @tasks = Task.where(user_id: params[:user_id], group_id: params[:user_id])
+          render json: @task.to_json
+        end
 
-  def by_group
-    @tasks = Task.where(group_id: params[:user_id])
-    render json: @task.to_json
-  end
+        def by_group
+          @tasks = Task.where(group_id: params[:user_id])
+          render json: @task.to_json
+        end
 
-  def by_pack
-    pack = PackOfTask.find_by_id(params[:pack_id])
-    @tasks = pack.tasks
-    render json: @task.to_json
+        def by_pack
+          pack = PackOfTask.find_by_id(params[:pack_id])
+          @tasks = pack.tasks
+          render json: @task.to_json
+        end
+      end
+    end
   end
 end
