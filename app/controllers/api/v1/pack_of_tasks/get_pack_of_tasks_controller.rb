08255fca4a7aa9a5720515dd9a4814f1a4d3cbe3
bposskuu <bposskuu@gmail.com>
2020-05-25 20:35:31 +0300

    change pack_of_tasks_controller and get_pack_of_tasks_controller

--- app/controllers/api/v1/pack_of_tasks/get_pack_of_tasks_controller.rb
+++ app/controllers/api/v1/pack_of_tasks/get_pack_of_tasks_controller.rb
@@ -1,13 +1,25 @@
 # frozen_string_literal: true
 
 module Api
-  module V1
-    module PackOfTasks
-      class GetPackOfTasksController < ApplicationController
-        def by_theme
-          tasks_id = Theme.where(id: params[:theme_id]).tasks.ids
-          @packs_of_task = PackOfTask.where(task_id: tasks_id)
-          render json: @packs_of_task.to_json
+    module V1
+      module PackOfTasks
+        class GetPackOfTasksController < ApplicationController
+            def by_theme
+              theme_id = Theme.select(:id).where(id: params[:theme_id])
+              tasks_ids = Task.where(theme_id: theme_id).ids
+              pack_of_task_ids = TasksInPack.select(:pack_of_task_id).where(task_id: tasks_ids)
+              @pack_of_task = PackOfTask.where(id: pack_of_task_ids)
+              render json: @pack_of_task.to_json
+            end
+
+            def by_course
+              category_ids = Category.select(:id).where(course_id: params[:course_id])
+              theme_ids = Theme.select(:id).where(category_id: category_ids)
+              tasks_ids = Task.where(theme_id: theme_ids).ids
+              pack_of_task_ids = TasksInPack.select(:pack_of_task_id).where(task_id: tasks_ids)
+              @pack_of_task = PackOfTask.where(id: pack_of_task_ids)
+              render json: @pack_of_task.to_json
+            end
         end
       end
     end
--- app/controllers/api/v1/pack_of_tasks/pack_of_tasks_controller.rb
+++ app/controllers/api/v1/pack_of_tasks/pack_of_tasks_controller.rb
@@ -1,25 +1,58 @@
 # frozen_string_literal: true
 
 module Api
-  module V1
-    module PackOfTasks
-      class PackOfTasksController < ApplicationController
-        before_action :set_pack_of_task, only: %i[show update destroy]
-
-        def index
-          render json: PackOfTask.all.to_json
-        end
-
-        def create
-          @pack_of_task = PackOfTask.new
-          @pack_of_task.user_id = params[:user_id]
-          @pack_of_task.name = params[:name]
-          @pack_of_task.description = params[:description]
-          if @pack_of_task.save
-            params[:task_ids].map { |task_id_one| TasksInPack.create pack_of_task_id: @pack_of_task.id, task_id: task_id_one }
-            show
-          else
-            render_error :bad_request, @pack_of_task.errors
+    module V1
+      module PackOfTasks
+        class PackOfTasksController < ApplicationController
+          before_action :set_pack_of_task, only: %i[show update destroy]
+  
+          def index
+            all_pack = PackOfTask.all
+            all_pack.map { |pack|
+              task_ids = TasksInPack.select(:task_id).where(pack_of_task_id: pack.id)
+              tasks = Task.where(id: task_ids)
+              pack.tasks = tasks
+            }
+            json_string = PackOfTaskSerializer.new(all_pack).serialized_json
+            render json: json_string
+          end
+  
+          def create
+            @pack_of_task = PackOfTask.new
+            @pack_of_task.user_id = params[:user_id]
+            @pack_of_task.name = params[:name]
+            @pack_of_task.description = params[:description]
+            if @pack_of_task.save
+              params[:task_ids].map{|task_id_one| TasksInPack.create :pack_of_task_id => @pack_of_task.id, :task_id => task_id_one}
+              show
+            else
+              render_error :bad_request, @pack_of_task.errors
+            end
+          end
+  
+          def show
+            task_ids = TasksInPack.select(:task_id).where(pack_of_task_id: @pack_of_task.id)
+            tasks = Task.where(id: task_ids)
+            #tasks = TasksInPack.where(pack_of_task_id: @pack_of_task.id).tasks
+            @pack_of_task.tasks = tasks
+            @json_string = PackOfTaskSerializer.new(@pack_of_task).serialized_json
+            render json: @json_string
+          end
+  
+          def update
+            if @pack_of_task.update(params[:user_id], params[:name], params[:description])
+              show
+            else
+              render_error :bad_request, @pack_of_task.errors
+            end
+          end
+  
+          def destroy
+            if @pack_of_task.destroy
+              render json: :ok, status: :ok
+            else
+              render_error :bad_request, @pack_of_task.errors
+            end
           end
         end
 
