7d419bb9f376d7c408e336fe09c4d985fa6286e5
Иван Гарцуев <l.tisha@mail.ru>
2020-06-08 21:02:56 +0300

    rem to_h

--- app/serializers/category_with_themes_serializer.rb
+++ app/serializers/category_with_themes_serializer.rb
@@ -3,26 +3,9 @@
 class CategoryWithThemesSerializer
   include FastJsonapi::ObjectSerializer
 
-  def to_h
-    data = serializable_hash
-
-    if data[:data].is_a? Hash
-      data[:data][:attributes]
-
-    elsif data[:data].is_a? Array
-      data[:data].map { |x| x[:attributes] }
-
-    elsif data[:data].nil?
-      nil
-
-    else
-      data
-    end
-  end
-
   set_type :category
   attributes :id, :course_id, :name, :description, :order_in_cource
   attribute :themes do |object|
-    ThemesSerializer.new(object.themes).to_h
+    ThemesSerializer.new(object.themes)
   end
 end
