3fa82f87261c2456c92bef467f51c46d08264401
Иван Гарцуев <l.tisha@mail.ru>
2020-06-19 15:29:32 +0300

    add auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@
 
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  before_action :authenticate_user!
 
   def bad_request
     render_error :bad_request, :bad_request
