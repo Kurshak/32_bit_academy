c76651a013f0976280fcb6912f7808c133562c71
bposskuu <bposskuu@gmail.com>
2020-06-21 17:28:07 +0300

    add order in by_task method in get_tests_controller

--- app/controllers/api/v1/tests/get_tests_controller.rb
+++ app/controllers/api/v1/tests/get_tests_controller.rb
@@ -5,7 +5,7 @@ module Api
       module Tests
         class GetTestsController < ApplicationController
           def by_task
-            @tests = Test.where(task_id: params[:task_id])
+            @tests = Test.where(task_id: params[:task_id]).order(:order_in_task)
             render json: TestSerializer.new(@tests)
           end
         end
